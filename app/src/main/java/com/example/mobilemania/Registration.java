5559ea4ce730b069186d0066bc0ff213571af0c3
TManeeshaR <maneesharajapaksha1@gmail.com>
2019-09-22 23:36:43 +0530

    Maneesha5

--- app/src/main/java/com/example/mobilemania/Registration.java
+++ app/src/main/java/com/example/mobilemania/Registration.java
@@ -51,7 +51,7 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         int numberCheck = cNumber1.length();
 
         if(!fname1.equals("") && !lname1.equals("") && !uname1.equals("") && !pass1.equals("") && !cpass1.equals("") && !address1.equals("") && !cNumber1.equals("")){
-            if(numberCheck > 10){
+            if(cNumber1.length() != 10){
                 Toast.makeText(this, "Please Enter A Valid Phone Number!", Toast.LENGTH_SHORT).show();
             }
             if(!pass1.equals(cpass1)){
