03e54fe490ba9c53bed1aba08ed148f11d6a756c
tchankamie <tharushachankamie97@gmail.com>
2019-09-24 00:14:00 +0530

    Add Validations

--- app/src/main/java/com/example/mobilemania/AddDeliverer.java
+++ app/src/main/java/com/example/mobilemania/AddDeliverer.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class AddDeliverer extends AppCompatActivity {
 
@@ -30,27 +31,61 @@ public class AddDeliverer extends AppCompatActivity {
         id  = findViewById(R.id.add_id);
         date    = findViewById(R.id.add_datec);
 
+
     }
 
 
     public void addCreditor(View view) {
 
-        String cname     = name.getText().toString();
-        String cphone    = phone.getText().toString();
-        String cid   = id.getText().toString();
-        String cdate     = date.getText().toString();
+        String cname = name.getText().toString();
+        String cphone = phone.getText().toString();
+        String cid = id.getText().toString();
+        String cdate = date.getText().toString();
 
-        dBhelper         = new DBhelper1(context);
-        sqLiteDatabase  = dBhelper.getWritableDatabase();
+        dBhelper = new DBhelper1(context);
+        sqLiteDatabase = dBhelper.getWritableDatabase();
 
-        dBhelper.addCreditorInfo(cname,cphone,cid,cdate,sqLiteDatabase);
+        if (cname.isEmpty() || cphone.isEmpty() || cid.isEmpty() || cdate.isEmpty()) {
+            if (cname.isEmpty()) {
+                name.setError("Name is Required!!!");
+            }
+            if (cphone.isEmpty()) {
+                phone.setError("Phone is Required!!!");
+            }
+            if (cid.isEmpty()) {
+                id.setError("Amount is Required!!!");
+            }
+            if (cdate.isEmpty()) {
+                date.setError("Date is Required!!!");
+            }
+            Toast.makeText(getApplicationContext(), "Empty field cannot add!!!", Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(getBaseContext(),"Data Saved",Toast.LENGTH_LONG).show();
+        } else if (isPhoneValid(cphone)) {
+            phone.setError("Invalid!!!");
+            Toast.makeText(getApplicationContext(), "Phone Number is Invalid!!!", Toast.LENGTH_SHORT).show();
+        } else {
 
-        dBhelper.close();
+            dBhelper.addCreditorInfo(cname, cphone, cid, cdate, sqLiteDatabase);
 
+            Toast.makeText(getBaseContext(), "Data Saved", Toast.LENGTH_LONG).show();
+
+            dBhelper.close();
+
+            Intent intent = new Intent(this, ListCreditors.class);
+            startActivity(intent);
+
+        }
     }
 
+    public static boolean isPhoneValid(String phoneNumber){
+        boolean valid = true;
+        String regex = "^(?:00255|\\+255|0)[6-9][0-9]{9}";
+
+        if (!phoneNumber.matches(regex)){
+            valid = false;
+        }
+        return valid;
+    }
     public void clearAll(View view){
         name.setText("");
         phone.setText("");
